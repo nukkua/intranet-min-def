201fe915e4c65b52a0f479db9ad38b518c475800
A.EibarSegales <andysejas444@gmail.com>
2024-07-25 16:59:18 -0400

    Inicio talwins

--- src/views/InicioPage.vue
+++ src/views/InicioPage.vue
@@ -1,9 +1,18 @@
 
-<template>
-    <Circulares/>;
+<template  >
+    <main class="flex min-h-screen w-full bg-[#5893D4]">
+        <div class=" p-8 border border-black " >
+            <img class="h-[150px]" src="https://sistemas.mindef.gob.bo/socorros/logo.jpg" alt="Ministerio de defensa LOGO">
+            <p>inicio</p>
+        </div>
+        <div class="m-10">
+            <h1>Intranet</h1>
+            <h2>Ministerio de Defensa</h2>
+        </div>
+        
+    </main>
 </template>
 
 <script setup>
     import Circulares  from './components/Circulares.vue';
-
 </script>
