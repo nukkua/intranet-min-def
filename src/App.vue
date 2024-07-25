dbab5fa9c6f887ff05ee370019394a56930b4c42
nukkua <marco.salazar.2014.com@gmail.com>
2024-07-25 15:31:18 -0400

    login page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
+import LoginPage from './views/LoginPage.vue';
 </script>
 
 <template>
-	<h1>Hola</h1>
-
+	<LoginPage />
 </template>
 
 <style scoped></style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,15 @@
+<template>
+	<main>
+		<h1>Intranet - Ministerio de Defensa</h1>
+		<input type="text" placeholder="Usuario" />
+		<input type="text" placeholder="Contrasena" />
+	</main>
+
+
+</template>
+<script lang="ts" setup>
+
+
+</script>
+
+<style scoped></style>
