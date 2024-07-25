86eb380b91b75be4472be1c8c26d06d688672ea5
A.EibarSegales <andysejas444@gmail.com>
2024-07-25 17:17:08 -0400

    veichan

--- src/views/InicioPage.vue
+++ src/views/InicioPage.vue
@@ -1,7 +1,7 @@
 
 <template  >
     <main class="flex min-h-screen w-full bg-[#5893D4]">
-        <div class=" p-8 border border-black " >
+        <div class=" p-8 border-8 border-black" >
             <img class="h-[150px]" src="https://sistemas.mindef.gob.bo/socorros/logo.jpg" alt="Ministerio de defensa LOGO">
             <p>inicio</p>
         </div>
