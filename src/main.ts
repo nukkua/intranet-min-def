21c24a60fab769e6d9e0af38daa300875b1debf2
nukkua <marco.salazar.2014.com@gmail.com>
2024-07-25 16:10:57 -0400

    vuetify added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import './main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import './main.css';
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
